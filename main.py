fcce9d7c44ee011a05ba72989ae9a784243b7623
Nicolas Freitas <n.freitas@unifesp.br>
2026-06-27 09:48:12 -0300

    implementei alguns dias da semana

--- main.py
+++ main.py
@@ -25,7 +25,36 @@ class EstratégiaSegundaFeira(EstratégiaDia):
                 f"Dia consultado: {dia_semana}\n"
                 f"Prioridade: {self.prioridade}\n"
                 f"Mensagem: {mensagem}")
+        
+class EstratégiaTercaFeira(EstratégiaDia):
+    prioridade = "ALTA"
+    def executar(self, usuario: str, info_adicional: str) -> str:
+        return f"avance nas tarefas pendentes. A meta é '{info_adicional}'."
 
+class EstratégiaQuartaFeira(EstratégiaDia):
+    prioridade = "MÉDIA"
+    def executar(self, usuario: str, info_adicional: str) -> str:
+        return f"dia de revisão, verifique o andamento da atividade '{info_adicional}'."
+
+class EstratégiaQuintaFeira(EstratégiaDia):
+    prioridade = "BAIXA"
+    def executar(self, usuario: str, info_adicional: str) -> str:
+        return f"colabore com alguém da equipe. inicie com '{info_adicional}'."
+
+class EstratégiaSextaFeira(EstratégiaDia):
+    prioridade = "MÉDIA"
+    def executar(self, usuario: str, info_adicional: str) -> str:
+        return f"registre o que foi concluído. pendencia: '{info_adicional}'."
+
+class EstratégiaSabado(EstratégiaDia):
+    prioridade = "BAIXA"
+    def executar(self, usuario: str, info_adicional: str) -> str:
+        return f"realize estudo livre ou descanso. o que planejei '{info_adicional}'."
+
+class EstratégiaDomingo(EstratégiaDia):
+    prioridade = "MÉDIA"
+    def executar(self, usuario: str, info_adicional: str) -> str:
+        return f"planeje a próxima semana. foque nessa semana em '{info_adicional}'."
 
 if __name__ == "__main__":
     estrategia_segunda = EstratégiaSegundaFeira()
